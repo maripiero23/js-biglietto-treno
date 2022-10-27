08222f47023807d1d33d17209b84baca7143e372
maripiero23 <mari.pieropan@gmail.com>
2022-10-27 15:45:27 +0200

    modifica2

--- js/main.js
+++ js/main.js
@@ -10,14 +10,20 @@ if (age < 18) {
     let discountPrice = priceJourney * .80
     let n = discountPrice.toFixed(2);
     console.log("prezzo scontato è", discountPrice.toFixed(2))
-    
+
 
 } else if (age > 65) {
     alert(priceJourney * .60);
     let discountPrice = priceJourney * .60
     let n = discountPrice.toFixed(2);
     console.log("prezzo scontato è", discountPrice.toFixed(2))
-}
+
+
+} else if (age >= 18 && age < 65) {
+    alert(priceJourney)
+ }
+
+
 
 
 
